78b3ea14b82da8b25943b19f4bc652f5bf128221
Gusmao021 <daniel.n.gusmao@hotmail.com>
2026-06-12 09:08:22 -0300

    Included all transitions in the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import HomePage from './pages/Home/HomePage'
@@ -7,15 +7,20 @@ import ArticlesPage from './pages/Articles/ArticlesPage'
 import ContactPage from './pages/Contact/ContactPage'
 
 export default function App() {
+  const location = useLocation()
+
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/quem-somos" element={<AboutPage />} />
-        <Route path="/artigos" element={<ArticlesPage />} />
-        <Route path="/contato" element={<ContactPage />} />
-      </Routes>
+      {/* key={pathname} remonta o wrapper a cada rota, reiniciando o fade */}
+      <div key={location.pathname} className="page-transition">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/quem-somos" element={<AboutPage />} />
+          <Route path="/artigos" element={<ArticlesPage />} />
+          <Route path="/contato" element={<ContactPage />} />
+        </Routes>
+      </div>
       <Footer />
     </>
   )
